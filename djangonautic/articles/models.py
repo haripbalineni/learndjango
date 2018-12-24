11bd44435a5f687ccfb2273380777eddf3abc315
haripbalineni <hbalineni@cce.soft.com>
2018-12-24 21:06:24 +0530

    push changes

--- djangonautic/articles/models.py
+++ djangonautic/articles/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length = 100)
+    slug = models.SlugField()
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    # Add a thumbnail later
+    # add a author later
+ 
+    def __str__(self):
+        return self.title
+
+
+
+# run these commands after any changes from model
+    # python manage.py makemigrations
+    # python manage.py migrate
